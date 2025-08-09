21af39a1f3110e8de62494ac021c45b6d46cfc05
anastaciomarbella <136615672+anastaciomarbella@users.noreply.github.com>
2025-08-08 23:47:29 -0600

    Update index.js
    se aniade ruta para consumir el api

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(helmet());
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://atempo-react.onrender.com'
+    'https://mi-api-atempo.onrender.com/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
