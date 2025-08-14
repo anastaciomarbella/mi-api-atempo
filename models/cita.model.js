364aae717bf2955f96370e6c989be46246194480
Marbella <anastaciomarbella19@gmail.com>
2025-08-14 15:44:59 -0600

    cambios en modal cita y register

--- models/cita.model.js
+++ models/cita.model.js
@@ -1,16 +1,14 @@
-// models/Cita.js
 function Cita(row) {
   return {
     id: row.id_cita || row.ID_CITA,
-    id_persona_uuid: row.id_persona_uuid || row.ID_PERSONA_UUID, // UUID string
+    id_persona: row.id_persona || row.ID_PERSONA,
     titulo: row.titulo || row.TITULO,
     fecha: row.fecha || row.FECHA,
     hora_inicio: row.hora_inicio || row.HORA_INICIO,
     hora_final: row.hora_final || row.HORA_FINAL,
     nombre_cliente: row.nombre_cliente || row.NOMBRE_CLIENTE,
     numero_cliente: row.numero_cliente || row.NUMERO_CLIENTE,
-    motivo: row.motivo || row.MOTIVO,
-    color: row.color || row.COLOR || null
+    motivo: row.motivo || row.MOTIVO
   };
 }
 
