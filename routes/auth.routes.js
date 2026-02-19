866bc9c6b25d84823a87230d57c6b65d21573dab
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-02-19 12:21:03 -0600

    cambios

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/auth.controller');
 const { verificarToken, verificarRol } = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.registrar);
