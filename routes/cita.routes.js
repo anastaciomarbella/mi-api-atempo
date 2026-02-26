afc14e3a2b6c8964824870bbd809f136810bdd8d
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-02-25 19:33:08 -0600

    cambios

--- routes/cita.routes.js
+++ routes/cita.routes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { verificarToken } = require("../midlewares/authMiddleware");
 const controller = require("../controllers/cita.controller");
 
-router.get("/citas", verificarToken, controller.obtenerCitas);
-router.post("/citas", verificarToken, controller.crearCita);
-router.put("/citas/:id", verificarToken, controller.actualizarCita);
-router.delete("/citas/:id", verificarToken, controller.eliminarCita);
+// ✅ Usar "/" ya que en index.js se hace app.use("/api/citas", router)
+router.get("/", verificarToken, controller.obtenerCitas);
+router.post("/", verificarToken, controller.crearCita);
+router.put("/:id", verificarToken, controller.actualizarCita);
+router.delete("/:id", verificarToken, controller.eliminarCita);
 
 module.exports = router;
