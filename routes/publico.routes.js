2da34d9857fb070c7c1e689df9b8efb8ad478325
anastaciomarbella <anastaciomarbella19@gmail.com>
2026-03-12 14:58:43 -0600

    cambios en logincss

--- routes/publico.routes.js
+++ routes/publico.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 
 const publicoController = require('../controllers/publico.controller');
 
-router.get('/:slug', publicoController.obtenerEmpresa);
-
-router.get('/:slug/personas', publicoController.obtenerPersonas);
-
-router.post('/:slug/citas', publicoController.crearCitaPublica);
+router.get('/:slug',                  publicoController.obtenerEmpresa);
+router.get('/:slug/personas',         publicoController.obtenerPersonas);
+router.get('/:slug/servicios',        publicoController.obtenerServiciosPublicos); // ← NUEVO
+router.get('/:slug/citas-ocupadas',   publicoController.obtenerCitasOcupadas);
+router.post('/:slug/citas',           publicoController.crearCitaPublica);
 
 module.exports = router;
